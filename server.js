47d2e14d84290322f313092ff5d3c6add6416006
margegaj24 <mario.margegaj@gmail.com>
2021-03-06 18:32:37 +0100

    Update

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const serverless = require("serverless-http");
 
 app.use(cors({ origin: "*" }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
 
-const studentRoute = require("./routes/student");
+const studentRoute = require("./routes/student.route.js");
 const courseRoute = require("./routes/course");
 
 app.use("/students", studentRoute);
 app.use("/courses", courseRoute);
-
-//app.get("/", (req, res) => res.status(200).json({ message: "Hello world" }));
-
-// const port = process.env.PORT;
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
 
 module.exports.handler = serverless(app);
