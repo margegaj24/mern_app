7a81cb68643fecc116abbddebc3f4867801501b6
margegaj24 <mario.margegaj@gmail.com>
2021-03-09 22:39:15 +0100

    Update

--- server.js
+++ server.js
@@ -3,13 +3,7 @@ const app = express();
 const cors = require("cors");
 const serverless = require("serverless-http");
 
-app.use(cors({ origin: "*" }));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
-});
+app.use(cors({ origin: "*", credentials: true }));
 
 const studentRoute = require("./routes/student.route.js");
 const courseRoute = require("./routes/course");
