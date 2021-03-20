80c8e5e3a0d73fbe19282aa14fde1ffac8384b1d
margegaj24 <mario.margegaj@gmail.com>
2021-03-20 22:17:33 +0100

    Update

--- lambdas/Course/deleteCurse.js
+++ lambdas/Course/deleteCurse.js
@@ -3,7 +3,7 @@ const { connectToMongoose } = require("../../db");
 
 module.exports.handler = async (event) => {
   connectToMongoose();
-  var data = await models.Course.findByIdAndDelete(event.params.id);
+  var data = await models.Course.findByIdAndDelete(event.pathParameters.id);
   return {
     statusCode: 200,
     body: JSON.stringify(data),
--- lambdas/Student/getStudent.js
+++ lambdas/Student/getStudent.js
@@ -1,13 +1,12 @@
-const db = require("../../models");
+const models = require("../../models");
 const { connectToMongoose } = require("../../db");
 
 module.exports.handler = async (event) => {
-    connectToMongoose();
-    var data = await db.Student.findById(event.params.id);
-    console.log(data);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  },
+  connectToMongoose();
+  var data = await models.Student.findById(event.pathParameters.id);
+  console.log(data);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 };
